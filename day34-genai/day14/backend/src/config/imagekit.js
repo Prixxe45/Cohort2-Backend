3c28efd326a6bddae3bd4e7a5558500e47847365
Prince Tripathi <prince45tri@gmail.com>
2026-06-12 04:19:27 +0530

    Initial commit

--- day34-genai/day14/backend/src/config/imagekit.js
+++
@@ -1,7 +0,0 @@
-const ImageKit = require("imagekit");
-
-const client = new ImageKit({
-  privateKey: process.env['IMAGEKIT_PRIVATE_KEY'],
-  publicKey: process.env['IMAGEKIT_PUBLIC_KEY'],
-  urlEndpoint: process.env['IMAGEKIT_URL_ENDPOINT']
-})
--- day34-genai/day8/Backend/src/models/note.modle.js
+++
@@ -1,9 +0,0 @@
- const mongoose = require("mongoose");
- 
- const noteSchema = new mongoose.Schema({
-    title: String,
-    content: String,
- });
-
- const NoteModel = mongoose.model("note", noteSchema);
- module.exports = NoteModel;
